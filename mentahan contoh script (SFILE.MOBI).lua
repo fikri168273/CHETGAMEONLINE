9651c9d1efa88a2f83a3099a10c38f34cf8a8d34
M.R F <72076100+fikri168273@users.noreply.github.com>
2020-09-30 20:51:41 +0700

    Sc mentahan

---
+++ mentahan contoh script (SFILE.MOBI).lua
@@ -0,0 +1,47 @@
+gg.alert("nama kalian", "Menu Cheat") --[[ 1 ]]
+gg.toast(" nama kalian ")
+while(true) do
+if gg.isVisible(true) then
+menuk = 1
+gg.setVisible(false)
+end
+
+
+START = 1
+function START()
+gg.alert(" nama kalian ")
+menu = gg.choice({
+"Auto Headshot"
+,"Damage++"
+,"Warna Tubuh"
+,"ðŸ”´ExitðŸ”´"
+},nil,"kasih teks sesuai keinginan kalian")
+if menu == 1 then DAK_AE1() end
+if menu == 2 then DAK_AE2() end
+if menu == 3 then DAK_AE3() end
+if menu == 4 then os.exit() end
+if menu == nil then else end
+menuk = -1
+end
+
+function DAK_AE1()
+
+
+end
+
+function DAK_AE2()
+
+
+end
+
+function DAK_AE3()
+
+
+end
+
+
+ print("Created By : M.R F")
+if menuk == 1 then START()
+  end
+end
+
